2cbce82d350b2e9f8e4afbf01128c78c43c21b59
MrMcLaine <Lypovskyi@gmail.com>
2023-11-03 23:22:14 +0200

    feat/ + AuthService

---
+++ _2_01_auth_api/src/services/AuthService.ts
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+interface Payload {
+    email: string;
+    code?: string;
+}
+
+class AuthService {
+    generateToken(data: Payload): string {
+        const payload = {
+            email: data.email
+        };
+
+        return jwt.sign(payload, process.env.JWT_SECRET!, { expiresIn: process.env.ACCESS_TOKEN_TTL });
+    }
+
+    generateRefreshToken(data: Payload): string {
+        const payload = {
+            email: data.email,
+            code: data.code
+        };
+
+        return jwt.sign(payload, process.env.JWT_SECRET!);
+    }
+}
+
+export const authService = new AuthService();
