4dedf81b70ec971a1f51003a681b80dbc0a2769c
MrMcLaine <Lypovskyi@gmail.com>
2023-11-03 23:57:15 +0200

    feat/ + return accessToken & refreshToken in login method in UserService

--- _2_01_auth_api/src/services/userService.ts
+++ _2_01_auth_api/src/services/userService.ts
@@ -1,14 +1,15 @@
 import pool from '../db';
-import { comparePassword, hashPassword } from "../utils/hashUtils";
-import { SQL_QUERIES } from "../constants";
-import { generateRandomCode } from "../utils/generateRandomCode";
-import { authService } from "./AuthService";
-
+import { comparePassword, hashPassword } from '../utils/hashUtils';
+import { SQL_QUERIES } from '../constants';
+import { generateRandomCode } from '../utils/generateRandomCode';
+import { authService } from './AuthService';
 
 const register = async (userData: { email: string; password: string }) => {
     const client = await pool.connect();
     try {
-        const { rows } = await client.query(SQL_QUERIES.FIND_USER_BY_EMAIL, [userData.email]);
+        const { rows } = await client.query(SQL_QUERIES.FIND_USER_BY_EMAIL, [
+            userData.email,
+        ]);
         if (rows.length) {
             return { success: false, message: 'User already exists.' };
         }
@@ -16,22 +17,31 @@ const register = async (userData: { email: string; password: string }) => {
         const hashedPassword = await hashPassword(userData.password);
         const refreshTokenCode = generateRandomCode(5);
 
-        const newUser = await client.query(SQL_QUERIES.INSERT_NEW_USER, [userData.email, hashedPassword, refreshTokenCode]);
+        const newUser = await client.query(SQL_QUERIES.INSERT_NEW_USER, [
+            userData.email,
+            hashedPassword,
+            refreshTokenCode,
+        ]);
 
         if (!newUser) {
             return { success: false, message: 'Failed to create user.' };
         }
 
-        const accessToken = authService.generateAccessToken({ email: userData.email });
-        const refreshToken = authService.generateRefreshToken({ email: userData.email, code: refreshTokenCode });
+        const accessToken = authService.generateAccessToken({
+            email: userData.email,
+        });
+        const refreshToken = authService.generateRefreshToken({
+            email: userData.email,
+            code: refreshTokenCode,
+        });
 
         return {
             success: true,
             data: {
                 id: newUser.rows[0].id,
                 accessToken: accessToken,
-                refreshToken: refreshToken
-            }
+                refreshToken: refreshToken,
+            },
         };
     } catch (error) {
         throw error;
@@ -43,7 +53,9 @@ const register = async (userData: { email: string; password: string }) => {
 const login = async (userData: { email: string; password: string }) => {
     const client = await pool.connect();
     try {
-        const { rows } = await client.query(SQL_QUERIES.FIND_USER_BY_EMAIL, [userData.email]);
+        const { rows } = await client.query(SQL_QUERIES.FIND_USER_BY_EMAIL, [
+            userData.email,
+        ]);
         const user = rows[0];
 
         if (!user) {
@@ -55,7 +67,22 @@ const login = async (userData: { email: string; password: string }) => {
             return { success: false, message: 'Invalid password.' };
         }
 
-        return { success: true, data: { id: user.id, email: user.email } };
+        const accessToken = authService.generateAccessToken({
+            email: userData.email,
+        });
+        const refreshToken = authService.generateRefreshToken({
+            email: userData.email,
+            code: user.refreshTokenCode,
+        });
+
+        return {
+            success: true,
+            data: {
+                id: user.id,
+                accessToken: accessToken,
+                refreshToken: refreshToken,
+            },
+        };
     } catch (error) {
         throw error;
     } finally {
@@ -65,5 +92,5 @@ const login = async (userData: { email: string; password: string }) => {
 
 export default {
     register,
-    login
+    login,
 };
